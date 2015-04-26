accba36a1d7a76e1f5bc325d56c5c1bcdf820fbb
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2015-04-26 17:25:46 +0900

    Change initialize

--- AirbnbViewController/AirbnbViewController.swift
+++ AirbnbViewController/AirbnbViewController.swift
@@ -153,11 +153,11 @@ public class AirbnbViewController: UIViewController, AirbnbMenuDelegate, AirbnbM
     public var topSession: AirbnbSessionView?
     public var middleSession: AirbnbSessionView?
     public var bottomSession: AirbnbSessionView?
-    public var lastIndexInSession: Dictionary<Int, Int>?
+    public var lastIndexInSession: Dictionary<Int, Int> = [0:0]
     public var thumbnailImages: [Dictionary<Int, UIImage>]?
     public var viewControllers: [Dictionary<Int, UIViewController>]?
     public var heightAirMenuRow: CGFloat?
-    
+ 
     override public init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
@@ -179,7 +179,6 @@ public class AirbnbViewController: UIViewController, AirbnbMenuDelegate, AirbnbM
         sessionViews = Dictionary<Int, AirbnbSessionView>()
         currentIndexSession = 0
         
-        lastIndexInSession = [0:0]
         currentIndexPath = NSIndexPath(forItem: 0, inSection: 0)
         
         self.delegate = self
@@ -235,7 +234,7 @@ public class AirbnbViewController: UIViewController, AirbnbMenuDelegate, AirbnbM
         self.currentIndexPath = indexPath
         
         if indexPath != nil && indexPath?.row != kIndexPathOutMenu.row {
-            self.lastIndexInSession?[indexPath!.section] = indexPath!.row
+            self.lastIndexInSession[indexPath!.section] = indexPath!.row
             self.saveViewControler(controller, atIndexPath: indexPath!)
         }
         
@@ -373,7 +372,7 @@ public class AirbnbViewController: UIViewController, AirbnbMenuDelegate, AirbnbM
             self.currentIndexSession = 0
         }
         
-        if let index = self.lastIndexInSession?[self.currentIndexSession!] {
+        if let index = self.lastIndexInSession[self.currentIndexSession!] {
             let lastIndexInThisSession: NSIndexPath = NSIndexPath(forRow:index, inSection: self.currentIndexSession!)
             let nextThumbnail: UIImage? = self.getThumbnailImageAtIndexPath(lastIndexInThisSession)
             if let image = nextThumbnail {
@@ -397,7 +396,7 @@ public class AirbnbViewController: UIViewController, AirbnbMenuDelegate, AirbnbM
             self.currentIndexSession = self.sessionViews!.count - 1
         }
         
-        if let index = self.lastIndexInSession?[self.currentIndexSession!] {
+        if let index = self.lastIndexInSession[self.currentIndexSession!] {
             let lastIndexInThisSession: NSIndexPath = NSIndexPath(forRow: index, inSection: self.currentIndexSession!)
             let prevThumbnail: UIImage? = self.getThumbnailImageAtIndexPath(lastIndexInThisSession)
             if let prev = prevThumbnail {
@@ -620,7 +619,7 @@ public class AirbnbViewController: UIViewController, AirbnbMenuDelegate, AirbnbM
     public func updateButtonColor() {
         for var i: Int = 0; i < self.sessionViews?.count; i++ {
             var sessionView: AirbnbSessionView? = self.sessionViews?[i]
-            var indexHighlight: Int? = self.lastIndexInSession![i]
+            var indexHighlight: Int? = self.lastIndexInSession[i]
             
             for object in sessionView!.containView!.allSubviews {
                 if object is UIButton {
@@ -661,7 +660,7 @@ public class AirbnbViewController: UIViewController, AirbnbMenuDelegate, AirbnbM
     
     public func rowDidTouch(button: UIButton) {
         // Save row touch in session
-        self.lastIndexInSession![self.currentIndexSession!] = button.superview!.tag
+        self.lastIndexInSession[self.currentIndexSession!] = button.superview!.tag
         
         self.currentIndexPath = NSIndexPath(forRow: button.tag, inSection: button.superview!.tag)
         
@@ -897,5 +896,251 @@ public class AirbnbViewController: UIViewController, AirbnbMenuDelegate, AirbnbM
         self.wrapperView?.removeFromSuperview()
         self.wrapperView = nil
         self.rowsOfSession = nil
+    }
+}
+
+public var SwipeTagHandle = "SWIPE_HANDER"
+public var SwipeObject = "SWIPE_OBJECT"
+
+public extension UIViewController {
+    
+    public var airSwipeGestureRecognizer: UISwipeGestureRecognizer? {
+        get {
+            var swipe: UISwipeGestureRecognizer? = objc_getAssociatedObject(self, &SwipeObject) as? UISwipeGestureRecognizer
+            if let sw = swipe {
+                return sw
+            } else {
+                swipe = UISwipeGestureRecognizer(target: self, action: "swipeHandler")
+                swipe?.direction = UISwipeGestureRecognizerDirection.Right
+                objc_setAssociatedObject(self, &SwipeObject, swipe, UInt(OBJC_ASSOCIATION_RETAIN_NONATOMIC))
+                return swipe
+            }
+            
+        }
+    }
+    
+    public var airSwipeHandler: airHandler? {
+        get {
+            // AnyObject -> id -> airHandler
+            return AirbnbHelper.usingAnyObjectWrapper(objc_getAssociatedObject(self, &SwipeTagHandle))
+        }
+        set {
+            if var obj: airHandler = newValue {
+                if let view = self.airSwipeGestureRecognizer?.view {
+                    view.removeGestureRecognizer(self.airSwipeGestureRecognizer!)
+                }
+                
+                if let nv = self.navigationController {
+                    nv.view.addGestureRecognizer(self.airSwipeGestureRecognizer!)
+                } else {
+                    self.view.addGestureRecognizer(self.airSwipeGestureRecognizer!)
+                }
+                // airHandler -> id -> AnyObject
+                objc_setAssociatedObject(self, &SwipeTagHandle, AirbnbHelper.usingClosureWrapper(obj), UInt(OBJC_ASSOCIATION_RETAIN_NONATOMIC))
+            } else {
+                if self.airSwipeGestureRecognizer?.view != nil {
+                    self.airSwipeGestureRecognizer?.view?.removeGestureRecognizer(self.airSwipeGestureRecognizer!)
+                }
+                if let ph = self.airSwipeGestureRecognizer?.view {
+                    ph.removeGestureRecognizer(self.airSwipeGestureRecognizer!)
+                }
+            }
+        }
+    }
+    
+    public var airViewController: AirbnbViewController {
+        get {
+            var parent: UIViewController = self
+            var parent2 = parent.parentViewController!
+            var parent3 = parent2.parentViewController!
+            print(parent)
+            return parent3 as! AirbnbViewController
+        }
+    }
+    
+    public func swipeHandler() {
+        if let handler: airHandler = self.airSwipeHandler {
+            handler()
+        }
+    }
+}
+
+//MARK: EXtension UIView
+
+public extension UIView {
+    
+    public var left: CGFloat {
+        get {
+            return self.frame.origin.x
+        }
+        set {
+            var frame: CGRect = self.frame
+            frame.origin.x = newValue
+            self.frame = frame
+        }
+    }
+    
+    public var top: CGFloat {
+        get {
+            return self.frame.origin.y
+        }
+        set {
+            var frame: CGRect = self.frame
+            frame.origin.y = newValue
+            self.frame = frame
+        }
+    }
+    
+    public var right: CGFloat {
+        get {
+            return self.frame.origin.x + self.frame.size.width
+        }
+        set {
+            var frame: CGRect = self.frame
+            frame.origin.x = newValue - frame.size.width
+            self.frame = frame
+        }
+    }
+    
+    public var bottom: CGFloat {
+        get {
+            return self.frame.origin.y + self.frame.size.height
+        }
+        set {
+            var frame: CGRect = self.frame
+            frame.origin.y = newValue - frame.size.height
+            self.frame = frame
+        }
+    }
+    
+    public var centerX: CGFloat {
+        get {
+            return self.center.x
+        }
+        set {
+            self.center = CGPointMake(newValue, self.center.y)
+        }
+    }
+    
+    public var centerY: CGFloat {
+        get {
+            return self.center.y
+        }
+        set {
+            self.center = CGPointMake(self.center.x, newValue)
+        }
+    }
+    
+    public var width: CGFloat {
+        get {
+            return self.frame.size.width
+        }
+        set {
+            var frame: CGRect = self.frame
+            frame.size.width = newValue
+            self.frame = frame
+        }
+    }
+    
+    public var height: CGFloat {
+        get {
+            return self.frame.size.height
+        }
+        set {
+            var frame: CGRect = self.frame
+            frame.size.height = newValue
+            self.frame = frame
+        }
+    }
+    
+    public var ttScreenX: CGFloat {
+        get {
+            var x: CGFloat = 0
+            var view: UIView? = self
+            for view; view == nil; view?.superview {
+                x += view!.left
+            }
+            return x
+        }
+    }
+    
+    public var ttScreenY: CGFloat {
+        get {
+            var y: CGFloat = 0
+            var view: UIView? = self
+            for (view; view == nil; view?.superview) {
+                y += view!.top
+            }
+            return y
+        }
+    }
+    
+    public var screenViewX: CGFloat {
+        get {
+            var x: CGFloat = 0
+            var view: UIView? = self
+            for (view; view == nil; view?.superview) {
+                x += view!.left
+                if view!.isKindOfClass(UIScrollView) {
+                    var scrollView: UIScrollView = view as! UIScrollView
+                    x -= scrollView.contentOffset.x
+                }
+            }
+            return x
+        }
+    }
+    
+    public var screenViewY: CGFloat {
+        get {
+            var y: CGFloat = 0
+            var view: UIView? = self
+            for (view; view == nil; view?.superview) {
+                y += view!.top
+                if view!.isKindOfClass(UIScrollView) {
+                    var scrollView: UIScrollView = view as! UIScrollView
+                    y -= scrollView.contentOffset.y
+                }
+            }
+            return y
+        }
+    }
+    
+    public var screenFrame: CGRect {
+        get {
+            return CGRectMake(self.screenViewX, self.screenViewY, self.width, self.height)
+        }
+    }
+    
+    public var origin: CGPoint {
+        get {
+            return self.frame.origin
+        }
+        set {
+            var frame: CGRect = self.frame
+            frame.origin = newValue
+            self.frame = frame
+        }
+    }
+    
+    public var size: CGSize {
+        get {
+            return self.frame.size
+        }
+        set {
+            var frame: CGRect = self.frame
+            frame.size = newValue
+            self.frame = frame
+        }
+    }
+    
+    public var allSubviews: NSArray {
+        get {
+            var arr: NSMutableArray = []
+            arr.addObject(self)
+            for subView in self.subviews {
+                arr.addObjectsFromArray(subView.allSubviews! as [AnyObject])
+            }
+            return arr
+        }
     }
 }
